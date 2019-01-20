3bee5584111076d311f6636de859aa82555d2502
Haziel <hajocava@gmail.com>
2019-01-20 00:40:39 -0600

    Clean code

--- server/src/graphql/resolvers/login.js
+++ server/src/graphql/resolvers/login.js
@@ -2,8 +2,6 @@ import { login } from '../../middleware/auth';
 
 export default {
     Mutation: {
-        login: (root, { email, password }) => {
-            return login(email, password);
-        }
+        login: (root, { email, password }) => login(email, password)
     }
 };
